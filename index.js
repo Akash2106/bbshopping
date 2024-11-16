4817f8a346ade1604d273bd42078da9fa5fef93d
akash <kakashkodi@gmail.com>
2024-11-16 15:35:03 +0530

    init

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express= require('express');
 const app = express();
+const cors = require('cors');
 const port= process.env.PORT || 4000;
 const routes = require('./route');
 const db=require('./DB');
 
+app.use(cors());
   app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   });
