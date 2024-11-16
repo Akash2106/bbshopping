613d094849e008bbb39712c2da7dbca22fa88336
akash <kakashkodi@gmail.com>
2024-11-16 14:15:03 +0530

    init

--- DB.js
+++ DB.js
@@ -2,9 +2,9 @@ const mysql=require('mysql2');
 
 // Create a new pool instance with your database configuration
 const pool = mysql.createPool({
-  user: 'root',         // Replace with your database username
-  host: 'localhost',            // Replace with your database host (e.g., localhost or an IP address)
-  database: 'akashdb',     // Replace with your database name
+  user: '2tzhgtK7527Pczp.root',         // Replace with your database username
+  host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',            // Replace with your database host (e.g., localhost or an IP address)
+  database: 'test',     // Replace with your database name
   password: '', // Replace with your database password 
 });
 
