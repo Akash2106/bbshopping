bd4d045e6e70e8619867fa91ff7713e4ddf2842b
akash <kakashkodi@gmail.com>
2024-11-21 12:26:04 +0530

    init

--- cart/cartService.js
+++ cart/cartService.js
@@ -6,7 +6,13 @@ const AddCart= async(req,res)=>{
         const [out] = await pool.query(`SELECT price FROM products WHERE productId=${productId};`);
         const Total=(out[0].price)*quantity;
         const [result]= await pool.query(`INSERT INTO cart(userId, productId, quantity,TotalPrice) VALUES (${userId},${productId},${quantity},${Total})`);
-        res.status(200).json(result);
+        const count=await pool.query(`SELECT COUNT(productId) AS TotalCount FROM cart WHERE userId=${userId}`);
+        const response={
+          message:'Cart item added successfully',
+          status:1,
+          count:count[0]
+        }
+        res.status(200).json(response);
       }
       catch (err){
         console.error(err);
@@ -18,7 +24,13 @@ const ViewCart= async(req,res)=>{
     try{
         const userId = req.params.id;
         const [result] = await pool.query(`SELECT * FROM cart RIGHT JOIN products ON cart.productId=products.productId WHERE test.cart.userId=${userId}`);
-        res.status(200).json(result);
+        const count=await pool.query(`SELECT COUNT(productId) AS TotalCount FROM cart WHERE userId=${userId}`);
+        const response={
+          message:'Cart item fetched successfully',
+          status:1,
+          count:count[0]
+        }
+        res.status(200).json(response);
       }
       catch(err){
         console.error(err);
@@ -31,7 +43,13 @@ const UpdateCart= async(req,res)=>{
     const {userId,productId,quantity}= req.body;
     if(quantity==0){
       const [result]=await pool.query(`DELETE FROM cart WHERE userId =${userId} AND productId=${productId}`);
-      res.status(200).json(result);
+      const count=await pool.query(`SELECT COUNT(productId) AS TotalCount FROM cart WHERE userId=${userId}`);
+      const response={
+        message:'Cart item removed successfully',
+        status:1,
+        count:count[0]
+      }
+      res.status(200).json(response);
     }
     else{
       const [out] = await pool.query(`SELECT price FROM products WHERE productId=${productId};`);
